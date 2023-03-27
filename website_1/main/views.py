0a8d4fa858672820b77bf5e2d79b8b1f81afe0d4
klen1980 <nick300880@gmail.com>
2023-03-27 22:39:47 +0300

    2_Homework

--- website_1/main/views.py
+++ website_1/main/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse ("<h4> This is index page </h4> <br> "
-                         "<p><a href='/homework'>Домашняя работа</a></p>")
+    return render (request, 'index.html')
 
 def homework (reqest):
-    return HttpResponse ('<h1> Why Httpresponse, why not render?????</h1>'
+    return HttpResponse ('<h1> Why Httpresponse, why not render????? <br> PS. Почему не сразу через render????</h1>'
                          ' <br>'
-                         ' <h2> Я думал со сменой лектора хоть что то  измениться, но я ошибался (надеюсь только пока </h2> '
+                         ' <h2> Я думал со сменой лектора хоть что то  измениться, но я ошибался (надеюсь только пока) </h2> '
                          '<br>'
-                         '<h2> Пусть хотя бы шрифт у себя увеличит!!!!! </h2> '
+                         '<h2> Пусть хотя бы шрифт у себя увеличит!!!!! <br> (понимаю что мне уже не поможет, но хоть следующим)</h2> '
                          '<h1><b> !!!!!ОРУУУУУУ В ГОЛОС!!!! </h1>')
 
